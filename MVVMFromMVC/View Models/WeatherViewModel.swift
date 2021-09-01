50791043e85597ae8fab4ad32955a127bca88941
Manuel Alejandro Verdugo <manuel.alejandro.verdugo@gmail.com>
2021-08-31 19:42:13 -0600

    Refactor del llamado de apis

--- MVVMFromMVC/View Models/WeatherViewModel.swift
+++ MVVMFromMVC/View Models/WeatherViewModel.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2019 Razeware LLC
+/// Copyright (c) 2021 Razeware LLC
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -18,6 +18,10 @@
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
 /// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,34 +30,46 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
+/*
+ First, add an import for UIKit.UIImage. No other UIKit types need to be permitted in the view model. A general rule of thumb is to never import UIKit in your view models.
+ Then, set WeatherViewModel‘s class modifier to public. You make it public in order for it to be accessible for testing.
 
-class WeatherViewController: UIViewController {
+ */
 
+// 1
+import UIKit.UIImage
+// 2
+public class WeatherViewModel {
+  private let geocoder = LocationGeocoder()
+  private static let defaultAddress = "Culiacán"
+  let locationName = Box("Loading...")
   
-  private let dateFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "EEEE, MMM d"
-    return dateFormatter
-  }()
-  private let tempFormatter: NumberFormatter = {
-    let tempFormatter = NumberFormatter()
-    tempFormatter.numberStyle = .none
-    return tempFormatter
-  }()
-  private let viewModel = WeatherViewModel()
-  
-  @IBOutlet weak var cityLabel: UILabel!
-  @IBOutlet weak var dateLabel: UILabel!
-  @IBOutlet weak var currentIcon: UIImageView!
-  @IBOutlet weak var currentSummaryLabel: UILabel!
-  @IBOutlet weak var forecastSummary: UITextView!
-  
-  override func viewDidLoad() {
-    viewModel.locationName.bind { [weak self] locationName in
-        self?.cityLabel.text = locationName
-      }
+  init() {
+    changeLocation(to: Self.defaultAddress)
   }
-
   
+  private func fetchWeatherForLocation(_ location: Location) {
+    WeatherbitService.weatherDataForLocation(
+      latitude: location.latitude,
+      longitude: location.longitude) { [weak self] (weatherData, error) in
+        guard
+          let self = self,
+          let weatherData = weatherData
+          else {
+            return
+          }
+    }
+  }
+  
+  func changeLocation(to newLocation: String) {
+    locationName.value = "Loading..."
+    geocoder.geocode(addressString: newLocation) { [weak self] locations in
+      guard let self = self else { return }
+      if let location = locations.first {
+        self.locationName.value = location.name
+        self.fetchWeatherForLocation(location)
+        return
+      }
+    }
+  }
 }
